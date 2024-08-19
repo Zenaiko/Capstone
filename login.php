713577063b2b2e235101f324675c1ac21e6d0f08
Zenaiko <navarromicoangelo@gmail.com>
2024-08-19 11:27:14 +0800

    Login

---
+++ login.php
@@ -0,0 +1,14 @@
+
+<form action="">
+    <div id="login_container">
+        <div id="login_field">
+            <input type="text" name="" id="" placeholder="Username/Email">
+            <div>
+                <input type="password" name="" id="" placeholder="Password">
+                <span id="show_password"><i class="bi bi-eye"></i></span>
+            </div>
+        </div>
+        <div id="forget_container"><a href="#" id="forget_pass">Forgot Password?</a></div>
+        <button id="login_submit" type="submit">Login</button>
+    </div>
+</form>
