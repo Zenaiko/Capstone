d73764de79a2d6b7bc2c42fe3aeee7a4abbac799
Mikalze <138299264+Mikalze@users.noreply.github.com>
2024-09-07 12:50:11 +0800

    navbar

---
+++ nav.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Store</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="stylesheet" href="nav.css">
+</head>
+<body>
+<!-- main nav -->
+<header class="header">
+    <a href="#" class="logo"><i class="fas fa-shopping-basket"></i></a>
+    <nav class="navbar">
+      
+              <a href="#">???</a>  
+              <a href="#">???</a>  
+              <a href="#">???</a>  
+              <a href="#">???</a>  
+              <a href="#">???</a>  
+              <a href="#">???</a>  
+        
+    </nav>
+
+    <div class="icons">
+      <div class="fas fa-search" id="search-btn"></div>
+      <div class="fas fa-shopping-cart" id="cart-btn"></div>
+      <div class="fas fa-message" id="msg-btn"></div>
+      <div class="fas fa-user" id="login-btn"></div>
+      <div class="fas fa-bars" id="menu-btn"></div>
+  </div>
+
+    <form action="" class="search-form">
+        <input type="search" id="search-box" placeholder="search here...">
+        <label for="search-box" class="fas fa-search"></label>
+    </form>
+
+</header>
+    <script src="nav.js"></script>
+</body>
+</html>
