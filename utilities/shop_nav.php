32f4a005c09a8469126afea1dc380955854225fd
Mikalze <138299264+Mikalze@users.noreply.github.com>
2024-09-10 09:24:01 +0800

    shop nav

---
+++ utilities/shop_nav.php
@@ -0,0 +1,14 @@
+
+<link rel="stylesheet" href="../css/shop_nav.css">
+<nav class="navbar">
+    <div class="nav-left">
+      <button class="back-btn">&#8592;</button>
+    </div>
+    <div class="nav-center">
+      <h1>My Cart</h1>
+    </div>
+    <div class="nav-right">
+      <button class="edit-btn">Edit</button>
+    </div>
+  </nav>
+  
