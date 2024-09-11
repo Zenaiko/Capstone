57eb748e8f7d0c1ab6dce538879850121476516b
Mikalze <138299264+Mikalze@users.noreply.github.com>
2024-09-11 10:54:53 +0800

    changes

---
+++ user_page/cart.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/cart.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php require_once('../utilities/initialize.php') ?>
+
+    <section id="cart_section">
+        <div id="cart_contents">
+
+            <div class="seller_cart_wrapper">
+                <div class="seller_cart_contents">
+                    <div class="seller_cart_name_container">
+                        <input type="checkbox" name="" id="" class="cart_seller_checkbox">
+                        <p class="seller_cart_name">Store 1</p>
+                    </div>
+                    <div class="seller_items_wrapper">
+                        <div class="seller_item">
+                            <input type="checkbox" name="" id="" class="item_cart_checkbox">
+                            <img src="../assets/tmp.png" alt="" class="item_cart_img">
+                            <div class="cart_item_description">
+                                <p class="item_cart_name">Item 01</p>
+                                <p class="item_cart_price item_price">₱20</p>
+                            </div>
+                            <div class="item_counter_wrapper">
+                                <div class="item_counter">
+                                    <i class="bi bi-dash"></i>
+                                    <p class="cart_item_qty">1</p>
+                                    <i class="bi bi-plus"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="seller_item">
+                            <input type="checkbox" name="" id="" class="item_cart_checkbox">
+                            <img src="../assets/tmp.png" alt="" class="item_cart_img">
+                            <div class="cart_item_description">
+                                <p class="item_cart_name">Item 01</p>
+                                <p class="item_cart_price item_price">₱20</p>
+                            </div>
+                            <div class="item_counter_wrapper">
+                                <div class="item_counter">
+                                    <i class="bi bi-dash"></i>
+                                    <p class="cart_item_qty">3</p>
+                                    <i class="bi bi-plus"></i>
+                                </div>
+                            </div>
+                        </div>
+                
+                    </div>
+                </div>
+            </div>
+
+            <hr class="splitter">
+
+            <div class="seller_cart_wrapper">
+                <div class="seller_cart_contents">
+                    <div class="seller_cart_name_container">
+                        <input type="checkbox" name="" id="" class="cart_seller_checkbox">
+                        <p class="seller_cart_name">Store 1</p>
+                    </div>
+                    <div class="seller_items_wrapper">
+                        <div class="seller_item">
+                            <input type="checkbox" name="" id="" class="item_cart_checkbox">
+                            <img src="../assets/tmp.png" alt="" class="item_cart_img">
+                            <div class="cart_item_description">
+                                <p class="item_cart_name">Item 01</p>
+                                <p class="item_cart_price item_price">₱20</p>
+                            </div>
+                            <div class="item_counter_wrapper">
+                                <div class="item_counter">
+                                    <i class="bi bi-dash"></i>
+                                    <p class="cart_item_qty">1</p>
+                                    <i class="bi bi-plus"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="seller_item">
+                            <input type="checkbox" name="" id="" class="item_cart_checkbox">
+                            <img src="../assets/tmp.png" alt="" class="item_cart_img">
+                            <div class="cart_item_description">
+                                <p class="item_cart_name">Item 01</p>
+                                <p class="item_cart_price item_price">₱20</p>
+                            </div>
+                            <div class="item_counter_wrapper">
+                                <div class="item_counter">
+                                    <i class="bi bi-dash"></i>
+                                    <p class="cart_item_qty">3</p>
+                                    <i class="bi bi-plus"></i>
+                                </div>
+                            </div>
+                        </div>
+                
+                    </div>
+                </div>
+            </div>
+
+            </div>
+        </div>
+    </section>
+</body>
+</html>
---
+++ utilities/item_interaction.php
@@ -0,0 +1,13 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="../css/item_interaction.css">
+
+<?php require_once('../utilities/initialize.php') ?>
+
+<div id="item_interaction_wrapper">
+    <div id="item_interaction_container">
+       <div id="item_interaction_share"><i class="bi bi-share"></i><p>Share</p></div>
+       <div id="item_interaction_message"><i class="bi bi-chat-dots"></i><p>Message</p></div>
+       <div id="item_interaction_cart" ><i class="bi bi-cart2"></i><p>Add to cart</p></div>
+       <div id="item_interaction_order"><p>Order Now</p></div>
+    </div>
+</div>
